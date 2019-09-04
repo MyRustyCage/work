b6d3dfd6577f21f1b67bf76b4c0061cf758e5336
MyRustyCage <guillaumebaylly@gmail.com>
2019-09-04 17:06:33 +0200

    Concatenated Js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
-var gulp = require('gulp');
-	gutil = require('gulp-util');
-	coffee = require('gulp-coffee');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee'),
+	concat = require ('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+	];
 
 gulp.task('coffee',function(done){
 	gulp.src(coffeeSources)
@@ -10,5 +17,12 @@ gulp.task('coffee',function(done){
 			.on('error',gutil.log))
 		.pipe(gulp.dest('components/scripts'))
 		done();
+})
+
+gulp.task ('js', function (done){
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
+	done();
 })
 
